ada5db61c97214d3ef95c7d49061b7b2026376cb
kikuKeii <kidevinfo73@gmail.com>
2021-01-27 20:48:21 +0700

    initial commit

---
+++ app/Views/pages/home.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('conten'); ?>
+<div class="row">
+    <div class="col-md-6 col-xl-3 mb-4">
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officia asperiores eos, cupiditate minima exercitationem natus modi excepturi. Earum tenetur placeat fuga animi explicabo. Error impedit deleniti fugit hic tempore.</p>
+    </div>
+</div>
+<?= $this->endSection(); ?>
